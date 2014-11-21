46b65658f43ba74041a6959df98cc7ead47995a1
Richard Machielse <richardmachielse@gmail.com>
2014-11-21 10:06:46 -0500

    WIP

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def package_data(pkg, roots):
 
 setup(
     name='xblockservicesdemo',
-    version='0.4',
+    version='0.5',
     description='XBlock Services Demo',
     packages=[
         'xblockservicesdemo',
